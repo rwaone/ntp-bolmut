18fb0da77050f13a9aa9bccaacc4b526ed9636bb
rwaone <rwaone@gmail.com>
2024-06-20 09:52:18 +0700

    add title in section

--- database/migrations/2024_06_05_032033_create_sections_table.php
+++ database/migrations/2024_06_05_032033_create_sections_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('sections', function (Blueprint $table) {
             $table->id();
             $table->foreignId('document_id');
+            $table->string('title');
             $table->string('name');
             $table->timestamps();
         });
--- database/seeders/SectionSeeder.php
+++ database/seeders/SectionSeeder.php
@@ -18,46 +18,57 @@ class SectionSeeder extends Seeder
         $sections = [
             [
                 'document_id' => 1,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA YANG DITERIMA PETANI',
             ],
             [
                 'document_id' => 1,
+                'title' => 'BLOK V',
                 'name' => 'V. HARGA YANG DIBAYAR PETANI',
             ],
             [
                 'document_id' => 2,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA YANG DITERIMA PETANI',
             ],
             [
                 'document_id' => 2,
+                'title' => 'BLOK V',
                 'name' => 'V. HARGA YANG DIBAYAR PETANI',
             ],
             [
                 'document_id' => 3,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA YANG DITERIMA PETANI',
             ],
             [
                 'document_id' => 3,
+                'title' => 'BLOK V',
                 'name' => 'V. HARGA YANG DIBAYAR PETANI',
             ],
             [
                 'document_id' => 4,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA YANG DITERIMA PETANI',
             ],
             [
                 'document_id' => 4,
+                'title' => 'BLOK V',
                 'name' => 'V. HARGA YANG DIBAYAR PETANI',
             ],
             [
                 'document_id' => 5,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA KONSUMEN DI PERDESAAN',
             ],
             [
                 'document_id' => 6,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA KONSUMEN DI PERDESAAN',
             ],
             [
                 'document_id' => 7,
+                'title' => 'BLOK IV',
                 'name' => 'IV. HARGA KONSUMEN DI PERDESAAN',
             ]
         ];
