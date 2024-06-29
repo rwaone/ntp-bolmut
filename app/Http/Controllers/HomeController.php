dfe09fc2779274dffd6d4ff605982376624bb838
rwaone <35188258+rwaone@users.noreply.github.com>
2024-06-29 14:15:42 +0800

    dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -22,7 +22,7 @@ class HomeController extends Controller
         ];
 
         return view('Index', [
-            'pageTitle' => 'Blank Page',
+            'pageTitle' => '',
             'breadcrumbItems' => $breadcrumbsItems
         ]);
     }
