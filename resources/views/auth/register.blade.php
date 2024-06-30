0b621bdda7d21f2362eda00274050db7af4308e9
rwaone <35188258+rwaone@users.noreply.github.com>
2024-06-30 17:39:28 +0800

    Update register.blade.php

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,7 +3,7 @@
         <div class="mobile-logo text-center mb-6 lg:hidden flex justify-center">
             <div class="mb-10 inline-flex items-center justify-center">
                 <x-application-logo />
-                <span class="ltr:ml-3 rtl:mr-3 text-xl font-Inter font-bold text-slate-900 dark:text-white">DashCode</span>
+                <span class="ltr:ml-3 rtl:mr-3 text-xl font-Inter font-bold text-slate-900 dark:text-white">Pengolahan NTP</span>
             </div>
         </div>
         <div class="text-center 2xl:mb-10 mb-4">
